ddd76bd35874cd64c76e24c0fe28bb3d357db94d
dcbarros <dcbarros1@gmail.com>
2024-03-28 20:42:45 -0300

    feat(dcbarros): Adiciona adição de estudantes na matéria.

--- src/main/java/br/com/professor/service/SubjectService.java
+++ src/main/java/br/com/professor/service/SubjectService.java
@@ -1,17 +1,26 @@
 package br.com.professor.service;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Set;
+
+import org.hibernate.ObjectNotFoundException;
 
 import br.com.professor.model.Student;
 import br.com.professor.model.Subject;
+import br.com.professor.repository.StudentRepository;
 import br.com.professor.repository.SubjectRepository;
 
 public class SubjectService {
 
     SubjectRepository subjectRepository;
+    StudentRepository studentRepository;
 
     public SubjectService(){
         this.subjectRepository = new SubjectRepository();
+        this.studentRepository = new StudentRepository();
     }
 
     public boolean save(Subject request){
@@ -26,6 +35,24 @@ public class SubjectService {
         }else{
             subjectRepository.updateAtomicTransaction(request);
         }
+        return true;
+    }
+
+    public boolean addStudentBySubjectCode(String studentCode, String subjectCode){
+
+        if(subjectCode == null || subjectCode.isBlank()) throw new IllegalArgumentException("O Código da matéria está inválida");
+        if(studentCode == null || studentCode.isBlank()) throw new IllegalArgumentException("A Matrícula do estudante está inválida");
+        
+        Student student = studentRepository.getById(studentCode);
+        Subject subject = subjectRepository.getById(subjectCode);
+
+        if(student == null) throw new NoSuchElementException("O Estudante deve ser cadastrado no sistema");
+        if(subject == null) throw new NoSuchElementException("A Matéria deve ser cadastrada no sistema");
+        
+        Set<Student> students = new HashSet<>(subject.getStudents());
+        students.add(student);
+        subject.setStudents(new ArrayList<>(students));
+        this.save(subject);
         return true;
     }
 
