bb07406e5ba373495df7e20626bd3180dfe07efa
dcbarros <dcbarros1@gmail.com>
2024-04-02 12:30:04 -0300

    feat(dcbarros): Adiciona modelo de mensagens que pode ser alterado entre os menus.

---
+++ src/main/java/br/com/professor/model/Message.java
@@ -0,0 +1,19 @@
+package br.com.professor.model;
+
+public class Message {
+    
+    private String text;
+
+    public Message(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+    
+}
